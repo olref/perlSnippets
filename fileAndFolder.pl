7ac3b8d42e8ff3d561b3191fb926f0ccaff42679
Olivier Refreges <olivier.refreges@gmail.com>
2012-08-27 11:43:14 +0200

    create fileAndFolder.pl and perlPostgreSQL.pl

---
+++ fileAndFolder.pl
@@ -0,0 +1,25 @@
+
+#list a CVS file recursively
+sub lsFolderRecursive {
+    my $dir = $_[0];
+    my $record;
+    my $path;
+    local *DIR;
+
+    unless ( opendir( DIR, $dir ) ) {
+        return;
+    }
+    while ( defined( $record = readdir(DIR) ) ) {
+        next
+          if ( $record eq "."
+            or $record eq ".."
+            or $record eq "CVS" );
+            $path = $dir . "/" . $record;
+        if ( -d $path ) {
+            lsFolderRecursive($path);
+        }
+        else {
+            push( @files, $path );
+        }
+    }
+}
