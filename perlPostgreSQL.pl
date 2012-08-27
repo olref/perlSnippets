8adf7019695f93d7a9230f991523628ebee0e46d
Olivier Refreges <olivier.refreges@gmail.com>
2012-08-27 11:44:03 +0200

    add perlPostgreSQL

---
+++ perlPostgreSQL.pl
@@ -0,0 +1,21 @@
+#postgreSQL with perl
+
+use DBI;
+
+my $dbDBIConnection="DBI:Pg:dbname=mydatabase;host=localhost;port=5432";
+my $dbUser="myUser";
+my $dbPass="passwd";
+
+#database connection
+my $dbh = DBI->connect($dbDBIConnection, $dbUser, $dbPass, {'RaiseError' => 1});
+
+#insert / delete and update
+my $update = "UPDATE/INSERT blablablabla;";
+my $rows = $dbh->do($update);
+
+
+#select
+my $select  = "SELECT blablabalbalbaabl";
+my $sth = $dbh->prepare($select);
+$sth->execute();
+my $ref = $sth->fetchrow_hashref();
